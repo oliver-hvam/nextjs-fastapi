cd3b95ca9a91d221f3849c4e36bcdac03aa9661e
hvamo <hvamo@DESKTOP-M90RKU2>
2025-02-05 17:40:25 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
-  const pyton_api_resopnse = await fetch("/api/py/helloFastApi");
+  const pyton_api_resopnse = await fetch("http://127.0.0.1:8000/api/py/docs");
 
   console.log(pyton_api_resopnse);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>{pyton_api_resopnse + ""}</p>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing FastApi API&nbsp;
