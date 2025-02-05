43f8f5816708e0d87c77594f16611ec328b9c3c5
hvamo <hvamo@DESKTOP-M90RKU2>
2025-02-05 16:57:18 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const pyton_api_resopnse = fetch("/api/py/helloFastApi");
+
+  console.log(pyton_api_resopnse);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
